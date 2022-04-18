9e629c84d8f8d208f5448373ee136a787da973a9
dong yeol <ehfql6363@naver.com>
2022-04-18 14:20:15 +0900

    Commit

---
+++ CH8/TNode.py
@@ -0,0 +1,5 @@
+class TNode():
+    def __init__(self, data, left, right):
+        self.data = data
+        self.left = left
+        self.right = right
---
+++ CH8/order.py
@@ -0,0 +1,29 @@
+from CH5.Queue import CircularQueue
+
+def preorder(n):
+    if n is not None:
+        print(n.data, end=' ')
+        preorder(n.left)
+        preorder(n.right)
+
+def inorder(n):
+    if n is not None:
+        inorder(n.left)
+        print(n.data, end=' ')
+        inorder(n.right)
+
+def postorder(n):
+    if n is not None:
+        postorder(n.left)
+        postorder(n.right)
+        print(n.data, end=' ')
+
+def levelorder(root):
+    queue = CircularQueue()
+    queue.enqueue(root)
+    while not queue.isEmpty():
+        n = queue.dequeue()
+        if n is not None:
+            print(n.data, end=' ')
+            queue.enqueue(n.left)
+            queue.enqueue(n.right)
