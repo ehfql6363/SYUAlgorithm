98cc53816fd7a8aff53665371111246d26768eed
dong yeol <ehfql6363@naver.com>
2022-05-01 16:08:31 +0900

    Commit

--- CH5/CircularDeque.py
+++ CH5/CircularDeque.py
@@ -1,25 +1,31 @@
 from CH5.Queue import CircularQueue
 
 MAX_QUEUE_SIZE = 10
+
+
 class CircularDeque(CircularQueue):
     def __init__(self):
         super().__init__()
 
-    def addRear(self, item): self.enqueue(item)
-    def deleteFront(self): self.dequeue()
-    def getFront(self): return self.peek()
+    def addRear(self, elem):
+        self.enqueue(elem)
+    def deleteFront(self):
+        return self.dequeue()
+    def getFront(self):
+        return self.peek()
 
-    def addFront(self, item):
+    def addFront(self, elem):
         if not self.isFull():
-            self.items[self.front] = item
-            self.front = self.front-1
-            if self.front < 0 : self.front = MAX_QUEUE_SIZE - 1
+            self.arr[self.front] = elem
+            self.front -= 1
+            if self.front < 0: self.front = MAX_QUEUE_SIZE - 1
 
     def deleteRear(self):
-        if not self.isFull():
-            item = self.rear
+        if not self.isEmpty():
+            elem = self.arr[self.rear]
             self.rear -= 1
-            if self.rear < 0 : MAX_QUEUE_SIZE - 1
+            if self.rear < 0: self.rear = MAX_QUEUE_SIZE - 1
+            return elem
 
     def getRear(self):
-        return self.items[self.rear]
+        return self.arr[self.rear]
--- CH5/PriorityQueue.py
+++ CH5/PriorityQueue.py
@@ -1,30 +1,32 @@
 class PriorityQueue():
     def __init__(self):
-        self.items = []
+        self.arr = []
 
     def isEmpty(self):
-        return len(self.items) == 0
-    def size(self): return len(self.items)
-    def clear(self): self.items = []
+        return len(self.arr) == 0
+    def size(self):
+        return len(self.arr)
+    def clear(self):
+        self.arr = []
 
-    def enqueue(self, item):
-        self.items.append(item)
+    def enqueue(self, elem):
+        self.arr.append(elem)
 
-    def findMaxIndex(self):
-        if self.isEmpty(): return None
+    def findMaxIndex(self): #배열 안의 값이 제일 큰 인덱스 찾기
+        if self.isEmpty(): return None #비어있으면 None 리턴
         else:
-            highest = 0
+            highest = 0 #초기 인덱스
             for i in range(1, self.size()):
-                if self.items[i] > self.items[highest]:
-                    highest = i
-            return highest
+                if self.arr[highest] < self.arr[i]: # highest인덱스의 배열 값과 비교하여
+                    highest = i # highest 인덱스를 배열의 값이 큰 인덱스로 갱신
+            return highest #highest 리턴
 
-    def deque(self):
+    def dequeue(self):
         highest = self.findMaxIndex()
         if highest is not None:
-            return self.items.pop(highest)
+            return self.arr.pop(highest)
 
     def peek(self):
         highest = self.findMaxIndex()
         if highest is not None:
-            return self.items[highest]
+            return self.arr[highest]
