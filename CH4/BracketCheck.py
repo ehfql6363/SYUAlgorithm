5ee6708fa3ee98c1838b13d418900a51510b2980
dong yeol <ehfql6363@naver.com>
2022-05-01 19:22:45 +0900

    Commit

--- CH4/BracketCheck.py
+++ CH4/BracketCheck.py
@@ -7,13 +7,13 @@ def bracketCheck(str):
         if ch in ('{', '(', '['):
             stack.push(ch)
         elif ch in ('}', ')', ']'):
-            if stack.isEmpty(): #괄호의 개수가 맞지 않다면 리턴
+            if stack.isEmpty(): #닫는 괄호가 먼저 왔다면 리턴
                 return False
             else:
                 left = stack.pop()
-                if (ch == '{' and left != '{') or \
-                        (ch == '(' and left != ')') or \
-                        (ch == '[' and left != ']'): #괄호의 종류가 맞지 않으면
+                if (ch == '}' and left != '{') or \
+                        (ch == ')' and left != '(') or \
+                        (ch == ']' and left != '['): #괄호의 종류가 맞지 않으면
                     return False
 
     return stack.isEmpty() #스택이 비어있는지 확인 (괄호의 개수 및 짝이 다 맞으면 Empty-True 아니면 False
